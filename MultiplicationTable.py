fd6508c307c91da2bfc89f667a0ffb0247a235ea
Mitesh Dhabekar <dev0330d@gmail.com>
2026-04-23 23:01:12 +0530

    first commit

---
+++ MultiplicationTable.py
@@ -0,0 +1,6 @@
+#User function Template for python3
+
+class Solution:
+    def getTable(self,n):
+        # code here
+        return [n * i for i in range(1, 11)]
---
+++ SumofNaturalNumbers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findSum(self, n):
+        # code here
+        if n == 0:
+            return 0
+        result = n*(n+1)/2
+        return (int(result))
+        
+        
